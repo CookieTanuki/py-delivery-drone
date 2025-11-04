50a5fb58d9508dc7805339bfe6f40f6a9b2d7747
Nick <nikitosi3012@gmail.com>
2025-11-04 14:20:58 +0200

    Improved code style

--- app/main.py
+++ app/main.py
@@ -9,11 +9,9 @@ class BaseRobot:
                  weight: int,
                  coords: list[int] | None = None) -> None:
 
-        if coords is None:
-            coords = [0, 0]
         self.name = name
         self.weight = weight
-        self.coords = coords
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -37,9 +35,9 @@ class FlyingRobot(BaseRobot):
                  weight: int,
                  coords: list[int] | None = None) -> None:
 
-        if coords is None:
-            coords = [0, 0, 0]
-        super().__init__(name, weight, coords)
+        coords = coords or [0, 0, 0]
+        super().__init__(name, weight, coords[:2])
+        self.coords = coords
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
